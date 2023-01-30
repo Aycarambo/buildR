9df43ccab002d3b80e90460bdde10f8c4e1bf690
Vigneron Thibault <thibault.vigneron@etudiant.univ-lr.fr>
2023-01-30 16:42:17 +0100

    init card

---
+++ front/src/components/Card.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <article>
+      <figure>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <figcaption>
+          <div></div>
+          <h2>ABC Paysagiste</h2>
+          <p>Angers</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Reiciendis, voluptas?
+          </p>
+        </figcaption>
+        <button></button>
+      </figure>
+
+      <div></div>
+    </article>
+  );
+};
+
+export default Card;
