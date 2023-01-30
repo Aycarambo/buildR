c3a22e55339bd30d38a9b52d656025b25884ddcd
Ay_Carambo <stan.lucas07@gmail.com>
2023-01-31 00:07:53 +0100

    categories

--- front/src/pages/EtapesInscription/Jobs.jsx
+++ front/src/pages/EtapesInscription/Jobs.jsx
@@ -3,20 +3,43 @@ import { useState, useEffect } from "react";
 import { DirectusContext } from "../../App";
 
 const Jobs = (props) => {
-  const { changePage } = props;
-
+  const { appendToState, changePage } = props;
   const [jobs, setJobs] = useState(null);
+  let selectedJobs = [];
+  if (jobs) {
+    jobs?.forEach((job) => {
+      if (job.selected) {
+        selectedJobs.push(job);
+      }
+    });
+  }
+
   const directus = useContext(DirectusContext);
 
   useEffect(() => {
     async function fetchJobs() {
       const res = await directus.items("jobs").readByQuery({ limit: -1 });
-      setJobs(res.data);
+      let tempJobs = [];
+      res.data.map((job, index) => {
+        tempJobs[index] = { index: index, label: job.label, selected: false };
+      });
+      setJobs(tempJobs);
     }
     fetchJobs();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const toggleSelectJob = (job) => {
+    const tempJobs = jobs;
+    tempJobs[job.index].selected = !tempJobs[job.index].selected;
+    setJobs([...tempJobs]);
+  };
+
+  const handleSubmit = () => {
+    appendToState({ categories: selectedJobs });
+    changePage(3);
+  };
 
   return (
     <>
@@ -32,8 +55,13 @@ const Jobs = (props) => {
           <ul className="categories">
             {jobs.map((job, idx) => {
               return (
-                <li key={idx} className="category">
-                  <button>
+                <li key={idx} className={"category"}>
+                  <button
+                    onClick={() => toggleSelectJob(job)}
+                    className={
+                      "button" + (job.selected ? " button--active" : "")
+                    }
+                  >
                     <img src="https://picsum.photos/200/300?random=2" alt="" />
                     <p>{job.label}</p>
                   </button>
@@ -44,12 +72,14 @@ const Jobs = (props) => {
         ) : (
           <p>Pas de données</p>
         )}
-        <button
-          onClick={() => changePage(3)}
-          className="btn choiceCategory__btn"
-        >
-          Suite
-        </button>
+        {selectedJobs.length > 0 && (
+          <button
+            onClick={() => changePage(3)}
+            className="btn choiceCategory__btn"
+          >
+            Suite
+          </button>
+        )}
       </main>
     </>
   );
