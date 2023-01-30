90d1abb17b650388bfff08c66928d6b926b076f7
Vigneron Thibault <thibault.vigneron@etudiant.univ-lr.fr>
2023-01-30 15:56:43 +0100

    fetch

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ConnexionInscription from "./pages/ConnexionInscription";
+import Jobs from "./pages/inscription/Jobs";
 import Layout from "./pages/Layout";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       </Routes>
       <Routes>
         <Route path="/connexion" element={<ConnexionInscription />} />
+        <Route path="/jobs" element={<Jobs />} />
       </Routes>
     </BrowserRouter>
   );
