5b3bd7d4b13bde9b9ddbed5fba2b51ac2fcf7339
Vigneron Thibault <thibault.vigneron@etudiant.univ-lr.fr>
2023-01-30 14:36:37 +0100

    reinit

--- front/src/pages/ConnexionInscription.jsx
+++ front/src/pages/ConnexionInscription.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import logoUrl from "../assets/images/logo.svg";
+import logoAppleUrl from "../assets/images/logoApple.png";
+import logoGoogleUrl from "../assets/images/logoGoogle.png";
 
 const ConnexionInscription = () => {
-  return <div>ConnexionInscription</div>;
+  return (
+    <main className="connexion">
+      <img src={logoUrl} alt="Logo Buildr" className="connexion__logo" />
+
+      <ul className="connexion__choises">
+        <li className="btn btn--yellow">
+          <button>Connexion</button>
+        </li>
+        <li className="btn btn--border">
+          <button>Inscriptions</button>
+        </li>
+      </ul>
+
+      <div className="connexion__with">
+        <button>
+          <img src={logoGoogleUrl} alt="connexion avec Google" />
+        </button>
+        <button>
+          <img src={logoAppleUrl} alt="connexion avec Apple" />
+        </button>
+      </div>
+
+      <p className="connexion__version">Version 1.0</p>
+    </main>
+  );
 };
 
 export default ConnexionInscription;
--- front/src/pages/Layout.jsx
+++ front/src/pages/Layout.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 
-const Layout = (props) => {
+const Layout = () => {
   return (
     <>
       <Header />
