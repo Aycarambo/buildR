7e1ca36bc8b2b2e21fcb209d952c57128204292a
Lafourcade Tanguy <tanguy.lafourcade@etudiant.univ-lr.fr>
2023-01-30 18:48:44 +0100

    Choix des categories

--- .history/front/src/App_20230130161648.jsx
+++ .history/front/src/App_20230130161648.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ConnexionInscription from "./pages/ConnexionInscription";
 import Jobs from "./pages/inscription/Jobs";
-import Home from "./pages/Home";
-import Likes from "./pages/Likes";
-import Messages from "./pages/Messages";
-import Account from "./pages/Account";
 import Layout from "./pages/Layout";
 
 const App = () => {
@@ -13,14 +9,12 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/likes" element={<Likes />} />
-          <Route path="/messages" element={<Messages />} />
-          <Route path="/account" element={<Account />} />
+          <Route index element={<ConnexionInscription />} />
         </Route>
       </Routes>
       <Routes>
         <Route path="/connexion" element={<ConnexionInscription />} />
+        <Route path="/choixcategories" element={<ChoixCategories />} />
         <Route path="/jobs" element={<Jobs />} />
       </Routes>
     </BrowserRouter>
