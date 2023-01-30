c904650702f1c59a28f7d57beb28f1f0bc485638
Ay_Carambo <stan.lucas07@gmail.com>
2023-01-30 18:59:25 +0100

    creation utilisateur dans bdd

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { createContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ConnexionInscription from "./pages/ConnexionInscription";
 import Home from "./pages/Home";
 import Likes from "./pages/Likes";
 import Messages from "./pages/Messages";
 import Account from "./pages/Account";
+import Inscription from "./pages/Inscription";
 import Layout from "./pages/Layout";
+import { Directus } from "@directus/sdk";
+
+export const DirectusContext = createContext();
+const directus = new Directus();
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/likes" element={<Likes />} />
-          <Route path="/messages" element={<Messages />} />
-          <Route path="/account" element={<Account />} />
-        </Route>
-      </Routes>
-      <Routes>
-        <Route path="/connexion" element={<ConnexionInscription />} />
-      </Routes>
+      <DirectusContext.Provider value={directus}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/likes" element={<Likes />} />
+            <Route path="/messages" element={<Messages />} />
+            <Route path="/account" element={<Account />} />
+          </Route>
+        </Routes>
+        <Routes>
+          <Route path="/connexion" element={<ConnexionInscription />} />
+          <Route path="/inscription" element={<Inscription />} />
+        </Routes>
+      </DirectusContext.Provider>
     </BrowserRouter>
   );
 };
