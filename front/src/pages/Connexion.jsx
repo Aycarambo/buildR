549678acfa4697464a235f8098ae026cb6ceeef3
Vigneron Thibault <thibault.vigneron@etudiant.univ-lr.fr>
2023-01-31 09:36:55 +0100

    Corrections scss

--- front/src/pages/Connexion.jsx
+++ front/src/pages/Connexion.jsx
@@ -44,23 +44,20 @@ const Connexion = () => {
         <h1 className="inscription-header__title">Connexion</h1>
         <h2 className="inscription-header__subtitle">Tu nous as manqué !</h2>
       </header>
-      <form className="authentification" onSubmit={(e) => handleSubmit(e)}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          className="authentification__input"
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          className="authentification__input"
-        />
-        <input type="submit" value="Connexion" />
-      </form>
+      <main className="authentification">
+        <div className="authentification__email authentification__input">
+          <input type="email" name="email" id="email" placeholder="Email" />
+        </div>
+        <div className="authentification__password authentification__input">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Mot de passe"
+          />
+        </div>
+        <button>Connexion</button>
+      </main>
       <aside className="authentification__other">
         <p className="or">Ou</p>
       </aside>
