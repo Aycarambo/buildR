147dadf010bbf0c60237f6bdef054f086b5d7c24
Lorel Guillaume <guillaume.lorel@etudiant.univ-lr.fr>
2023-01-31 01:47:46 +0100

    Guillaume

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import Card from "../components/Card";
+import yellowHeart from "../assets/images/yellow-heart.svg";
+import btnClose from "../assets/images/btn-close-overlay.svg";
 
 const Home = () => {
-  return <Card />;
+  return (<>
+  <main className="home">
+  <Card />
+  <div className="home__containerBtn">
+    <button className="btnSwipe btnSwipe--declined">
+      <img src={btnClose} alt="" />
+    </button>
+    <button className="btnSwipe btnSwipe--accept">
+      <img src={yellowHeart} alt="" />
+    </button>
+  </div>
+  </main>
+
+  </>
+  );
 };
 
 export default Home;
